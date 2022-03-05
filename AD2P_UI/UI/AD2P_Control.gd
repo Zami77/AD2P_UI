b238c084d24704f3aba5eedd584d2a2800444ad7
Daniel Lungaro <chunk_77@hotmail.com>
2022-03-05 14:30:24 -0800

    chore: removed submodule

--- AD2P_UI/UI/AD2P_Control.gd
+++ AD2P_UI/UI/AD2P_Control.gd
@@ -1,8 +1,13 @@
 extends Control
 
+
+
 func _ready():
 	pass # Replace with function body.
 
+func ad2p_scan_file(path: String):
+	# TODO: send http request to backend with file.
+	pass
 
 func _on_FileDialog_file_selected(path: String):
-	print(path)
+	ad2p_scan_file(path)
